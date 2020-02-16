74a531808642a794b3e0156bade828ce5db3257e
Lee, Dong Hyun <sos0911@korea.ac.kr>
2020-02-16 22:07:59 +0900

    Second Chapter completed
    implement bullet spawner, update player movement, etc,.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 8f;
+    private Rigidbody bulletrigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletrigidbody = GetComponent<Rigidbody>();
+        // 방향은 bullet 기준 앞 방향으로 설정됨
+        // 속도만 rigidbody에서 설정해주면 자기가 알아서감
+        // 이건 처음알았네..
+        bulletrigidbody.velocity = transform.forward * speed;
+
+        // 일정 시간 뒤 파괴됨
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 총알끼리 부딪힐 수도 있으므로 태그 검사
+        if (other.tag == "Player")
+        {
+            Debug.Log("conflict!");
+            // 만약 getcomponent 결과가 없다면 null이 저장됨
+            PlayerController playercontroller = other.GetComponent<PlayerController>();
+
+            if (playercontroller != null)
+                playercontroller.Die();
+        }
+    }
+}
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+
+    public GameObject bulletprefab;
+    public float spawnRateMin = 0.5f; // 탄알 최소생성주기
+    public float spawnRateMax = 3f; // 탄알 최대생성주기
+
+    private Transform target; // 탄알이 날아갈 대상의 위치
+    private float spawnRate; // 생성주기
+    private float timeAfterSpawn; // 최근 생성 후 경과시간
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 처음에는 탄알을 쏘지 않았으므로 0f
+        timeAfterSpawn = 0f;
+        // min <= spawnrate < max
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        // PlayerController 타입의 오브젝트 하나를 scene에서 찾아 tranform 컴포넌트를 caching
+        // 어차피 player는 맵상에 하나밖에 존재하지 않음
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 각 프레임 간의 시간 = deltatime
+        // ex : 120fps라면 deltatime=1/120
+        timeAfterSpawn += Time.deltaTime;
+
+        if (timeAfterSpawn >= spawnRate)
+        {
+            // 탄알 생성
+            timeAfterSpawn = 0f;
+
+            GameObject bullet = Instantiate(bulletprefab, transform.position, transform.rotation);
+            // 생성된 탄알이 player를 보게 회전
+            bullet.transform.LookAt(target);
+            // 한번 만들어낸 후에는 간격이 랜덤으로 설정됨
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
+    }
+}
