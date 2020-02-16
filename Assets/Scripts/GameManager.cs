501d26c7a9cb287feb56ddfbd997d36bd57d65e4
Lee, Dong Hyun <sos0911@korea.ac.kr>
2020-02-16 22:45:18 +0900

    Third chapter ongoing
    implement map rotation, UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    // gameovertext는 실시간으로 내용이 변경될 필요가 없으므로 게임오브젝트로 선언함
+    public GameObject gameovertext;
+    public TextMeshProUGUI timetext;
+    public TextMeshProUGUI recordtext;
+
+    private float survivetime;
+    private bool isGameover; // 게임이 끝났는가?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        survivetime = 0;
+        isGameover = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGameover)
+        {
+            // 1초에 1씩 증가하게 됨
+            survivetime += Time.deltaTime;
+            // 초단위로 끊어서 표현
+            timetext.text = "Time : " + (int)survivetime;
+        }
+    }
+}
---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+
+    public float rotationSpeed = 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // y축 기준으로 돌려버리기
+        // time.deltatime을 곱했으므로 결국 1초에 rotationspeed만큼 회전하게 됨
+        transform.Rotate(0f, rotationSpeed*Time.deltaTime, 0f);
+
+    }
+}
